604a7cab95b9ca72cf8267af0f61ae02f52e03bb
seiren-primrose <seirenprimrose@gmail.com>
2025-04-19 13:41:57 +0800

    Add files via upload

---
+++ lib/index.d.ts
@@ -0,0 +1,12 @@
+//index.d.ts
+import makeWASocket from './Socket';
+export * from '../WAProto';
+export * from './Utils';
+export * from './Types';
+export * from './Store';
+export * from './Defaults';
+export * from './WABinary';
+export * from './WAM';
+export type WASocket = ReturnType<typeof makeWASocket>;
+export { makeWASocket };
+export default makeWASocket;
